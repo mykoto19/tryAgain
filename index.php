b099da36dec19583bd6af8a5acd2a8dba0955614
mykoto19 <maxwelltormon@gmail.com>
2020-10-17 16:43:46 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<title>GSC_BAC Information</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+</head>
+<body>
+
+	<form action="process.php" method="post">
+		Enter your name: <input name="name" type="text">
+		<input type="submit">
+	</form>
+
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Processing..</title>
+</head>
+<body>
+
+	<?php
+
+		$name = $_POST["name"];
+		echo "Hi, " . $name;
+
+	?>
+
+</body>
+</html>
